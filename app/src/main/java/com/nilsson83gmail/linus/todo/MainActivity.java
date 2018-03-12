bf4e8dc18b3ac40c3470d0590f4098a6d428b1bb
PerLinus <linus.nilsson83@gmail.com>
2018-03-12 14:12:39 +0100

    Some more classes

--- app/src/main/java/com/nilsson83gmail/linus/todo/MainActivity.java
+++ app/src/main/java/com/nilsson83gmail/linus/todo/MainActivity.java
@@ -2,12 +2,26 @@ package com.nilsson83gmail.linus.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Buttons
+        findViewById(R.id.toDo).setOnClickListener(this);
+        findViewById(R.id.shoppingLists).setOnClickListener(this);
+        findViewById(R.id.passwords).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/nilsson83gmail/linus/todo/models/Todo.java
+++ app/src/main/java/com/nilsson83gmail/linus/todo/models/Todo.java
@@ -1,5 +1,6 @@
 package com.nilsson83gmail.linus.todo.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,8 +10,63 @@ import java.util.List;
 public class Todo {
 
     private String name;
-    private List todoList;
+    private List<String> todoList;
+    private List<String> list1 = new ArrayList<>();
+    private List<String> list2 = new ArrayList<>();
+
+
+    public Todo(String name, List<String> todoList) {
+        this.name = name;
+        this.todoList = todoList;
+        list1.add("Måla");
+        list1.add("Slipa");
+        list1.add("Spika");
+        list1.add("Skruva");
+        list1.add("Bygga");
+
+        list2.add("Plugga");
+        list2.add("Städa");
+        list2.add("Spela");
+        list2.add("Betala räkningar");
+        list2.add("Diska");
+        list2.add("Programmera");
+        list2.add("Kolla mejl");
+        list2.add("Ringa mamma");
+        list2.add("Ringa pappa");
+        list2.add("Läsa bok");
+        list2.add("Ta en promenad");
+        list2.add("Sova lite");
+        list2.add("Tvätta");
+        list2.add("Dricka vatten");
+        list2.add("Kolla avtal");
+        list2.add("Baka");
+        list2.add("Laga mat");
+        list2.add("Handla");
+        list2.add("Uppdatera datorn");
+        list2.add("skaffa jobb");
+
+    }
 
 
 
+    public static final Todo[] toDos = {
+            new Todo("Ska göra", new ArrayList<String>())
+    };
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getTodoList() {
+        return todoList;
+    }
+
+    public void setTodoList(List<String> todoList) {
+        this.todoList = todoList;
+    }
 }
